eee74faaa4514221370de501c9086915b536db81
vaslim <mislav.jancic@protonmail.com>
2025-04-26 22:02:34 +0200

    Update deps

--- src/main/java/org/vaslim/subtitle_fts/service/impl/IndexServiceImpl.java
+++ src/main/java/org/vaslim/subtitle_fts/service/impl/IndexServiceImpl.java
@@ -11,7 +11,6 @@ import net.openhft.hashing.LongHashFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
