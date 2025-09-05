fb6b19ab1cec59a9e28f8010b2b6ecad4127732e
vaslim <mislav.jancic@protonmail.com>
2025-09-05 17:32:55 +0200

    Test delete and restart

--- src/main/java/org/vaslim/subtitle_fts/SubtitleFtsApplication.java
+++ src/main/java/org/vaslim/subtitle_fts/SubtitleFtsApplication.java
@@ -1,13 +1,28 @@
 package org.vaslim.subtitle_fts;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SubtitleFtsApplication {
 
+    private static ConfigurableApplicationContext context;
 	public static void main(String[] args) {
 		SpringApplication.run(SubtitleFtsApplication.class, args);
 	}
+
+    public static void restart() {
+        ApplicationArguments args = context.getBean(ApplicationArguments.class);
+
+        Thread thread = new Thread(() -> {
+            context.close();
+            context = SpringApplication.run(SubtitleFtsApplication.class, args.getSourceArgs());
+        });
+
+        thread.setDaemon(false);
+        thread.start();
+    }
 
 }
